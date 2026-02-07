b5cdffdc9d8e8558b6bc109d409484c827d2e6d8
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-07 09:53:28 +0500

    solved question 5 of Day 05 with better approach

--- Day 03/secondLargest.js
+++ Day 03/secondLargest.js
@@ -1,14 +1,5 @@
 function secondLargest(arr){
-   let newArr=[];
-   const firstLargest=Math.max(...arr);
-   for(let i=0;i<arr.length;i++){
-      if(arr[i]===firstLargest){
-        continue;
-      }
-      newArr.push(arr[i]);
-   }
-   const secondLargest=Math.max(...newArr);
-   return secondLargest;
+   return arr.sort((a,b)=>b-a)[1];
 
 }
 console.log(secondLargest([1,2,9,3,4,5]));
