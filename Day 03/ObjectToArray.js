59ccef92e7b91615a7753494cc8f05d222e0850f
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-07 05:28:30 +0500

    added another solution for question 1 Day 03

--- Day 03/ObjectToArray.js
+++ Day 03/ObjectToArray.js
@@ -1,7 +1,15 @@
-function toArray(obj){
+function toArray1(obj){
     return Object.entries(obj);
 }
-console.log(toArray({
+console.log(toArray1({
     "age":2,
     "class":10
 }));
+function toArray2(obj){
+    let arr=[];
+    Object.keys(obj).forEach((key)=>{
+        arr.push([key,obj[key]]);
+    })
+    return arr;
+}
+console.log(toArray2({"name":"Raheel","age":21}));
