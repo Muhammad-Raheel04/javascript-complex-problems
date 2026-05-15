a5f301437dfa15f48171a7b5c9507b3d4d5890cc
Muhammad Raheel <raheel31102004@gmail.com>
2026-05-15 19:33:39 +0500

    remove unused files

--- Day 01/redundant.js
+++
@@ -1,7 +0,0 @@
-function redundant(str){
-    return function(){
-        return str;
-    }
-}
-const f1=redundant('apples');
-console.log(f1());
--- Day 01/trueCount.js
+++
@@ -1,4 +0,0 @@
-function countTrue(arr){
-    return arr.filter((element)=>element===true).length;
-}
-console.log(countTrue([false,false,true]));
